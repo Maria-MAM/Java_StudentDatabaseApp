14462cc39a2229c9f15f96004bb8581105327fcc
Maria-MAM <mariamihaila1409@gmail.com>
2020-02-25 21:05:22 +0200

    first commit

---
+++ StudentDatabaseApp.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class StudentDatabaseApp {
+    public static void main(String [] args) {
+
+        System.out.println("Enter no of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+        
+        for (int n = 0; n < numOfStudents; n++) {
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition(); 
+        }
+        
+        for (int n = 0; n < numOfStudents; n++) {
+            System.out.println(students[n].toString());
+        }
+        
+    }
+}
